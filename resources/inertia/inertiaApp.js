6cecb58e89c0cf0dc8cebab0b51a833ca93dcc1f
wahyu <nueg44@gmail.com>
2025-11-28 10:39:34 +0700

    feat: setup tanstack + pinia and update packages

--- resources/inertia/inertiaApp.js
+++ resources/inertia/inertiaApp.js
@@ -11,14 +11,32 @@ import StyleClass from 'primevue/styleclass';
 import Avatar from 'primevue/avatar';
 import {Menubar} from "primevue";
 import './Assets/main.css';
+import { createPinia } from 'pinia';
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query';
+
+// global config for tanstack query
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: false,
+            refetchOnWindowFocus: false,
+            staleTime: 5 * 60 * 1000, // 5 Minutes
+        },
+    },
+});
+
+
 createInertiaApp({
     resolve: name =>
         resolvePageComponent(`./Modules/${name}.vue`, import.meta.glob('./Modules/**/*.vue')),
     setup({ el, App, props, plugin }) {
-
+        const pinia = createPinia()
         const app = createApp({ render: () => h(App, props) });
-
         app.use(plugin);
+        app.use(pinia);
+        app.use(VueQueryPlugin, {
+            queryClient,
+        })
         app.use(PrimeVue, {
             theme: {
                 preset: Aura,
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/tess', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index']);
